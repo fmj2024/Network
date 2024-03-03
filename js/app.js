f4c926031ec720e0b946887201b73c6473da99ec
amyhean <amyburzumburton@gmail.com>
2024-03-03 05:48:23 +0300

    Datos

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ check.addEventListener('click', idioma);
 function idioma() {
     let id=check.checked;
     if (id==true) {
-        location.href= "../es/esin.html";
+        location.href= "https://fmj2024.github.io/NetworEsp/";
     } else{
         location.href = "../index.html";
     }
